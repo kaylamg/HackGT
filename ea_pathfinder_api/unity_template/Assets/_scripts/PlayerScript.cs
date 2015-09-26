22f5d5b0a2749c7d513faa34a5e8d0d6c9d797d2
kaylamg <kaylamg@stanford.edu>
2015-09-26 06:07:04 -0400

    worked on controller fixes in unity

--- ea_pathfinder_api/unity_template/Assets/_scripts/PlayerScript.cs
+++ ea_pathfinder_api/unity_template/Assets/_scripts/PlayerScript.cs
@@ -25,11 +25,15 @@ public class PlayerScript : MonoBehaviour {
 
 	void positionHandler (ControllerMessage msg){
 		ctlrNum = msg.ControllerSource;
-		float x = int.Parse(msg.Payload.GetField ("x-coordinate").ToString()) * Screen.width;
-		float y = int.Parse (msg.Payload.GetField ("y-coordinate").ToString()) * Screen.height;
-		Vector3 newPos = new Vector3 (x, y, (controllers [ctlrNum - 1].transform.position.z));
-		Debug.Log (newPos);
-		//controllers [ctlrNum - 1].transform.position (newPos);
+		//Debug.Log (float.Parse(msg.Payload.GetField ("x-coordinate").ToString ()));
+		float x = float.Parse (msg.Payload.GetField ("x-coordinate").ToString ()) * 100;
+		float y = float.Parse (msg.Payload.GetField ("y-coordinate").ToString()) * 205;
+		//Debug.Log (controllers [ctlrNum - 1].transform.position.z);
+		//Vector3 newPos = (x, y, (controllers [ctlrNum - 1].transform.position.z));
+		//Debug.Log (newPos);
+		controllers [ctlrNum - 1].transform.localPosition = new Vector3 (x, y, 0);
+		Debug.Log ("X POSITION: " + x);
+		Debug.Log ("Y POSITION: " + y);
 	}
 
 
