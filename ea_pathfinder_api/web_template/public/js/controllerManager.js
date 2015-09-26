ea1ed2b5dd4c58efd4dcbfeee2cbbd731df15954
mwillcox99 <mwillcox99@gmail.com>
2015-09-26 06:45:39 -0400

    Color change on touch

--- ea_pathfinder_api/web_template/public/js/controllerManager.js
+++ ea_pathfinder_api/web_template/public/js/controllerManager.js
@@ -14,20 +14,16 @@ $(document).ready(function () {
 	conn = new Connection();
 	conn.sendMessage({"type": "connect"});
 
-	//Process incoming game messages
-	// $(document).on("game_message", function (e, message) {
-	// 	console.log("Received Message: " + JSON.stringify(message));
-	// 	var payload = message.payload;
-	// 	switch (payload.type) {
-	// 		//your code here
-	// 	}
-	// });
+	$("body").on('touchstart', function (e) {
+		$("body").addClass('touched');
+	});
+
+	$("body").on('touchend', function (e) {
+		$("body").removeClass('touched');
+	});
 
 	$("body").on('touchmove', function (e) {
 		e.preventDefault();
-		console.log("entered touchmove")
-
-		console.log(e);
 
 		touchobj = e.originalEvent.changedTouches[0];
 		var touchX = parseInt(touchobj.clientX) / screen.height; // height and width switched in landscape view for mobile!
@@ -42,4 +38,3 @@ $(document).ready(function () {
 		conn.sendMessage(msg, 0);
 	});
 });
-
