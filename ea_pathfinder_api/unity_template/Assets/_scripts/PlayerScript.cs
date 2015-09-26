1e9ff1a384dcf9e3875bee868e2e51a3a38f1743
kaylamg <kaylamg@stanford.edu>
2015-09-26 05:33:43 -0400

    fixes in controller script

--- ea_pathfinder_api/unity_template/Assets/_scripts/PlayerScript.cs
+++ ea_pathfinder_api/unity_template/Assets/_scripts/PlayerScript.cs
@@ -25,8 +25,8 @@ public class PlayerScript : MonoBehaviour {
 
 	void positionHandler (ControllerMessage msg){
 		ctlrNum = msg.ControllerSource;
-		int x = msg.Payload.GetField ("x-coordinate") * Screen.width;
-		int y = msg.Payload.GetField ("y-coordinate") * Screen.height;
+		float x = int.Parse(msg.Payload.GetField ("x-coordinate").ToString()) * Screen.width;
+		float y = int.Parse (msg.Payload.GetField ("y-coordinate").ToString()) * Screen.height;
 		Vector3 newPos = new Vector3 (x, y, (controllers [ctlrNum - 1].transform.position.z));
 		Debug.Log (newPos);
 		//controllers [ctlrNum - 1].transform.position (newPos);
