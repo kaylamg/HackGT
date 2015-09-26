db5a85fcee4e45f95784ec3dab4c1e9f741d2d24
mwillcox99 <mwillcox99@gmail.com>
2015-09-26 04:18:40 -0400

    Initializes controllers

--- ea_pathfinder_api/web_template/public/js/controllerManager.js
+++ ea_pathfinder_api/web_template/public/js/controllerManager.js
@@ -1,9 +1,9 @@
 /*
 Copyright (C) 2015 Electronic Arts Inc.  All rights reserved.
- 
+
 This software is solely licensed pursuant to the Hackathon License Agreement,
 Available at:  www.eapathfinders.com/license
-All other use is strictly prohibited. 
+All other use is strictly prohibited.
 */
 
 $(document).ready(function () {
@@ -13,14 +13,27 @@ $(document).ready(function () {
 	// INIT..
 	conn = new Connection();
 	conn.sendMessage({"type": "connect"});
-	
-	// Process incoming game messages
-	$(document).on("game_message", function (e, message) {
-		console.log("Received Message: " + JSON.stringify(message));
-		var payload = message.payload;
-		switch (payload.type) {
-			//your code here
-		}
+
+	//Process incoming game messages
+	// $(document).on("game_message", function (e, message) {
+	// 	console.log("Received Message: " + JSON.stringify(message));
+	// 	var payload = message.payload;
+	// 	switch (payload.type) {
+	// 		//your code here
+	// 	}
+	// });
+
+	$("body").on('touchmove', function (e) {
+		e.preventDefault();
+		console.log("entered touchmove")
+
+		//send message to unity
+		// var msg = {
+		// 	"type": "position",
+		// 	"x-coordinate": x,
+		// 	"y-coordinate": y
+		// };
+		// conn.sendMessage(msg, 0);
 	});
 });
 
