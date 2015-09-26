0b7f38d68c7d09952b0ce7ca71216c52c9986149
kaylamg <kaylamg@stanford.edu>
2015-09-26 03:48:22 -0400

    Refactors Scripts

---
+++ ea_pathfinder_api/unity_template/Assets/_scripts/ballCollide.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballCollide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("************* entered the trigger");
+		if (other.tag == "wall") {
+			Destroy (gameObject);
+		}
+	}
+
+}
