1a2cc371ee30e022962d79015877c951f3659f3b
kaylamg <kaylamg@stanford.edu>
2015-09-25 23:49:30 -0400

    Initial Setup

---
+++ ea_pathfinder_api/unity_template/Assets/BallScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour {
+
+	public GameObject [] balls;
+
+	private int ballSpeed = 5;
+	private bool startGame = false;
+
+	// Use this for initialization
+	void Start () {
+
+		//ask user if they want to start the game
+		startGame = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (startGame == true) {
+			balls [1].transform.localPosition += transform.right * ballSpeed * Time.deltaTime;
+		}
+	
+	}
+}
