ba66e7c245cc69139fc9a6fe86f6f4ec0907be30
eniasebiomo <eni.asebiomo@gmail.com>
2015-09-26 21:52:50 -0400

    Reworks Level Creator Flow

--- ea_pathfinder_api/unity_template/Assets/_scripts/LevelScript.cs
+++ ea_pathfinder_api/unity_template/Assets/_scripts/LevelScript.cs
@@ -1,13 +1,25 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class LevelScript : MonoBehaviour {
 
 	public int playerLevel;
+	public Text endGameText;
 
 	public GameObject ballCollideScript;
 
 	public GameObject ballPrefab;
+
+	public GameObject ULP;
+	public GameObject URP;
+	public GameObject LLP;
+	public GameObject LRP;
+
+	public GameObject OrangeGoal;
+
+	public int lives;
+	public int currentBall;
 
 	//array of gae objects with walls and obstacles
 	
@@ -15,21 +27,12 @@ public class LevelScript : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		playerLevel = 0;
+		lives = 5;
 		ballCollideScript = GameObject.Find ("ballMoveScript");
-
-
+		setupLevel ();
+	
 		restartGame ();
 
-		//ask players if they are ready to start
-
-//		switch (playerLevel) {
-//		case (0):
-			//ballArray [0].transform.position = new Vector3(Screen.width / 2, Screen.height/2, ballArray [0].transform.position.z);
-			//ballCollideScript.GetComponent<ballCollide> ().ballSpeed = 2f;
-			//ballCollideScript.GetComponent<ballCollide> ().ballDirection = 3;
-			//ballCollideScript.GetComponent<ballCollide> ().startGame = true;
-//			break;
-//		}
 	
 	}
 	
@@ -39,45 +42,99 @@ public class LevelScript : MonoBehaviour {
 	}
 
 	public void win () {
-		Debug.Log ("in win!");
-		increaseLevel ();
+		currentBall++;
 		restartGame ();
+		
 	}
 
 	public void lose() {
+		lives--;
 		restartGame ();
 	}
 
 	// Detects the level and sets up the scene etc. 
 	public void restartGame () {
-		Debug.Log ("playerLevel" + playerLevel);
+		if (lives <= 0) 
+			endGame ();
+
 		switch (playerLevel) {
+		
 		case(0):
-			GameObject ball = (GameObject) Instantiate(ballPrefab, transform.position = new Vector3(0, 0, 0), Quaternion.identity);
-			ball.GetComponent<ballCollide>().ballDirection = 2;
+			switch(currentBall) {
+			case(0):
+				GameObject ball = (GameObject) Instantiate(ballPrefab, transform.position = new Vector3(0, 0, -1), Quaternion.identity);
+				ball.GetComponent<ballCollide>().ballDirection = 1;
+				break;
+			case(1):
+				ball = (GameObject) Instantiate(ballPrefab, transform.position = new Vector3(0, 0, -1), Quaternion.identity);
+				ball.GetComponent<ballCollide>().ballDirection = 3;
+				break;
+			case(2):
+				ball = (GameObject) Instantiate(ballPrefab, transform.position = new Vector3(-6.5f, 4.34f, -1), Quaternion.identity);
+				ball.GetComponent<ballCollide>().ballDirection = 2;
+				break;
+			case(3):
+				ball = (GameObject) Instantiate(ballPrefab, transform.position = new Vector3(6, -4.28f, -1), Quaternion.identity);
+				ball.GetComponent<ballCollide>().ballDirection = 4;
+				break;
+			case(4):
+				increaseLevel();
+				break;
+			}
+
 			break;
 		case(1):
-//			ballCollideScript.GetComponent<ballCollide>().ballDirection = 3;
-//			Debug.Log ( ballCollideScript.GetComponent<ballCollide>().ballDirection);
 
+			break;
 
-			ball = (GameObject) Instantiate(ballPrefab, transform.position = new Vector3(0, 0, 0), Quaternion.identity);
-			ball.GetComponent<ballCollide>().ballDirection = 1;
-			Debug.Log ( ball.GetComponent<ballCollide>().ballDirection);
-			
+		
+		}
+	}
 
+	//Increase level by 1
+	public void increaseLevel() {
+		destroyLevel ();
+		playerLevel++;
+		setupLevel ();
+	}
+
+	public void destroyLevel() {
+		Destroy (ULP);
+		Destroy (URP);
+		Destroy (LLP);
+		Destroy (LRP);
+
+		switch (playerLevel) {
+		case(0):
+			Destroy(OrangeGoal);
 			break;
 		}
 
 	}
 
-	//Increase level by 1
-	public void increaseLevel() {
-		playerLevel++;
-		Debug.Log ("increasing level!");
+	public void setupLevel() {
+		currentBall = 0;
+
+		switch (playerLevel) {
+		
+		case(0):
+
+			Instantiate (ULP, transform.position = new Vector3 (-4, 2, -1), Quaternion.identity);
+			Instantiate (URP, transform.position = new Vector3 (4, 2, -1), Quaternion.identity);
+			Instantiate (LLP, transform.position = new Vector3 (-4, -3, -1), Quaternion.identity);
+			Instantiate (LRP, transform.position = new Vector3 (4, -3, -1), Quaternion.identity);
+			
+			Instantiate (OrangeGoal, transform.position = new Vector3 (-0.16f, -4.45f, -0.52f), Quaternion.identity);
+			break;
+
+		case(1):
+			break;
+		}
 	}
 
-	private void level1() {
-
+	public void endGame() {
+		endGameText.enabled = false;
+		Debug.Log ("You Lost!");
 	}
+
 }
