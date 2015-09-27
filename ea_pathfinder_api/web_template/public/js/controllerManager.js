cd47f8aaac9c7b8066a0e80235d7b2d7c8570a9e
mwillcox99 <mwillcox99@gmail.com>
2015-09-27 05:01:23 -0400

    Sounds DONE!

--- ea_pathfinder_api/web_template/public/js/controllerManager.js
+++ ea_pathfinder_api/web_template/public/js/controllerManager.js
@@ -7,39 +7,23 @@ All other use is strictly prohibited.
 */
 
 $(document).ready(function () {
-	startAudio = new Audio ("sound/electric-power-turn-on.mp3");
-	touchMoveAudio = new Audio ("sound/swish.mp3");
-	touchEndAudio = new Audio ("sound/blop.wav"); // buffer on load as soon as controller
+
+	console.log ("Document Loaded");
 
 	// INIT..
 	conn = new Connection();
 	conn.sendMessage({"type": "connect"});
 
-	// MENU, display start screen
+	// SOUNDS
+	startAudio = new Audio ("sound/electric-power-turn-on.mp3");
+	touchEndAudio = new Audio ("sound/peeeooop.wav");
+
+	// START SCREEN
 	var gameInProgress = 0;
 	$("body").addClass('start');
 
-	$("body").on('touchstart', function (e) {
-		e.preventDefault();
-		if (!gameInProgress) {
-			startAudio.play();
-
-			// send msg to unity
-			var msg = { "type": "start" };
-			conn.sendMessage(msg, 0);
-
-			// GAME BEGINS, display game screen
-			gameInProgress = 1;
-			$("body").removeClass('start');
-		}
-		$("body").addClass('touched');
-
-	});
-
 	$("body").on('touchmove', function (e) {
 		e.preventDefault();
-		touchMoveAudio.play();
-
 		touchobj = e.originalEvent.changedTouches[0];
 		var touchX = parseInt(touchobj.clientX) / screen.height; // height and width switched in landscape view for mobile!
 		var touchY = parseInt(touchobj.clientY) / screen.width;
@@ -53,9 +37,43 @@ $(document).ready(function () {
 		conn.sendMessage(msg, 0);
 	});
 
-	$("body").on('touchend', function (e) {
-		e.preventDefault();
-		$("body").removeClass('touched');
+	$("body").on('touchstart', function (e) {
+		$("body").addClass('touched');
 		touchEndAudio.play();
 	});
+
+	$("body").on('touchend', function (e) {
+		e.preventDefault();
+		if (!gameInProgress) {
+			startAudio.play();
+			touchEndAudio.play();
+			// send msg to unity
+			var msg = { "type": "start" };
+			conn.sendMessage(msg, 0);
+
+			// GAME BEGINS, display game screen
+			gameInProgress = 1;
+			$("body").removeClass('start');
+		}
+		touchEndAudio.play();
+		$("body").removeClass('touched');
+	});
+
+	$("body").on('mousedown', function (e) {
+		e.preventDefault();
+		if (!gameInProgress) {
+			startAudio.play();
+			touchEndAudio.play();
+			// send msg to unity
+			var msg = { "type": "start" };
+			conn.sendMessage(msg, 0);
+
+			// GAME BEGINS, display game screen
+			gameInProgress = 1;
+			$("body").removeClass('start');
+		}
+		touchEndAudio.play();
+		$("body").removeClass('touched');
+	});
+
 });
